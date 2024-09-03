d8a0345f5bf515a33d648eb613f3368813499477
Lakshay <102456284+Monkfuego@users.noreply.github.com>
2024-09-04 00:39:46 +0530

    Update test.py

--- PIIDetective/test.py
+++ PIIDetective/test.py
@@ -1,6 +1,6 @@
 from pii_detective import pii_detect
 
 
-detected_pii = pii_detect('Adhr_crdSubro.pdf')
+detected_pii = pii_detect('Put the fucking file name here')
 for label, match in detected_pii:
     print(f"Detected {label}: {match}")
