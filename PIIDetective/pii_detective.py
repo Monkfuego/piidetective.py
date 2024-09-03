cc283538956a915fce9aa781cb5a240575009f7d
Lakshay <102456284+Monkfuego@users.noreply.github.com>
2024-09-04 01:35:43 +0530

    Update pii_detective.py

--- PIIDetective/pii_detective.py
+++ PIIDetective/pii_detective.py
@@ -4,13 +4,15 @@ from pii_regex import pii_regex,doc_pii_regex
 
 def pii_detect(file):
     results = []
-    
+    text = read_text_from_file(file)
+    if isinstance(text, list):
+        text = ' '.join(text)
     for label, pattern in {**pii_regex, **doc_pii_regex}.items():
-        matches = pattern.findall(read_text_from_file(file))
+        matches = pattern.findall(text)
         if matches:
             for match in matches:
                 results.append((label, match))
-    
     return results
+
 
 
